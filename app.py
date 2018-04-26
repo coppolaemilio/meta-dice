980dc2c9f70715f504a13203ffc5d36fe5c0bc4c
Emilio Coppola <coppolaemilio@gmail.com>
2018-04-26 11:50:33 +0200

    cant invalidate on Discord

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 import flask
-import string
 import random
 app = flask.Flask(__name__)
-
-def id_generator(size=20, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
 @app.route('/')
 def index():
     return flask.redirect('/6')
 
 @app.route('/<max>')
-def simple_roll(max):
-    return flask.redirect('/roll/{max}/{random}'.format(max=max, random = id_generator()))
-
-@app.route('/roll/<max>/<id>')
-def roll(max, id):
+def roll(max):
     number = random.randint(1, int(max))
     return """
     <!DOCTYPE html>
@@ -27,10 +19,9 @@ def roll(max, id):
     </head>
     <body>
         <h1>{number}</h1>
-        <p style="display: none">{id}</p>
     </body>
     </html>
-    """.format(number = str(number), id = id)
+    """.format(number=str(number))
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
